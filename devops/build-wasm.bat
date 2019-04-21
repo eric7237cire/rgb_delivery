cfc89013a9d95adb57c47bfe154771493da84778
Eric <eric7237cire@gmail.com>
2019-04-21 17:27:37 +0200

    better watch

--- devops/build-wasm.bat
+++ devops/build-wasm.bat
@@ -1,6 +1,6 @@
 
 SET DEVOPS_DIR=%~dp0
-SET WEB_WORKER_BUILD_DIR=%DEVOPS_DIR%..\web_worker_build\
+SET WEB_WORKER_BUILD_DIR=%DEVOPS_DIR%..\web_worker\
 SET GRID_EDITOR_BUILD_DIR=%DEVOPS_DIR%..\grid-editor\
 SET RGB_SOLVER_DIR=%~dp0..\rgb-solver\
 
@@ -9,8 +9,15 @@ cd /D "%RGB_SOLVER_DIR%"
 REM wasm-pack build --dev
 wasm-pack build --release
 
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 CD "%WEB_WORKER_BUILD_DIR%"
 DEL "%GRID_EDITOR_BUILD_DIR%src\assets\*.wasm"
 RMDIR /S /Q "%WEB_WORKER_BUILD_DIR%dist"
 CALL .\node_modules\.bin\webpack
-CALL "%DEVOPS_DIR%copy-wasm-build-files.bat"
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+copy /Y "%WEB_WORKER_BUILD_DIR%dist\*"  "%GRID_EDITOR_BUILD_DIR%src\assets"
+copy /Y "%WEB_WORKER_BUILD_DIR%worker\interface_types.ts" "%GRID_EDITOR_BUILD_DIR%src\app\typings\worker.ts
+
--- devops/wasm_webworker_watch.bat
+++ devops/wasm_webworker_watch.bat
@@ -1,4 +1,6 @@
 SET DEVOPS_DIR=%~dp0
 SET RGB_SOLVER_DIR=%~dp0..\rgb-solver\
+
 cd /D "%RGB_SOLVER_DIR%"
-cargo watch -i .gitignore -i "pkg/*" -s "%DEVOPS_DIR%build-wasm.bat" -w ../wasm-typescript-definition -w .
+
+cargo watch --debug  -i .gitignore -i "pkg/*" -s "%DEVOPS_DIR%build-wasm.bat" -w ../wasm-typescript-definition -w . -w "../web_worker/worker"
