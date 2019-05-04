fb899bbd6562da2a4dd24bb59805f3f0abb4b0da
Eric <314512+eric7237cire@users.noreply.github.com>
2019-05-04 21:57:40 +0200

    print max ticks

--- web_worker/worker/worker.ts
+++ web_worker/worker/worker.ts
@@ -54,6 +54,7 @@ ctx.addEventListener("message", ev => {
         }
         case RequestTypes.INIT_CALCULATIONS: {
             g_worker.universe.init_calculate();
+            console.log("Init calculations, max ticks: ", requestMessage.maxSteps);
             g_worker.universe.set_max_ticks(requestMessage.maxSteps);
             g_worker.reloadGridData();
             break;
