9047585d3f9e63db29abdd7e430297ceb50901d6
Eric <314512+eric7237cire@users.noreply.github.com>
2019-05-07 21:12:09 +0200

    simplify modules

--- grid-editor/src/app/log.service.ts
+++ grid-editor/src/app/log.service.ts
@@ -6,17 +6,33 @@ export const enum LogLevel {
   Info = 2,
   Warn = 3,
   Error = 4,
-  Fatal = 5,
   Off = 6
 }
 
 export class LogEntry {
   // Public Properties
-  entryDate: Date = new Date();
+
   message: string = "";
-  level: LogLevel = LogLevel.Debug;
+  level: LogLevel = LogLevel.All;
   extraInfo: any[] = [];
   logWithDate: boolean = true;
+
+
+
+  private static formatParams(params: any[]): string {
+    let ret: string = params.join(",");
+
+    // Is there at least one object in the array?
+    if (params.some(p => typeof p === "object")) {
+      ret = "";
+      // Build comma-delimited string
+      for (const item of params) {
+        ret += JSON.stringify(item) + ",";
+      }
+    }
+
+    return ret;
+  }
 
   buildLogString(): string {
     let ret: string = "";
@@ -29,21 +45,6 @@ export class LogEntry {
     if (this.extraInfo.length) {
       ret += " - Extra Info: "
         + LogEntry.formatParams(this.extraInfo);
-    }
-
-    return ret;
-  }
-
-  private static formatParams(params: any[]): string {
-    let ret: string = params.join(",");
-
-    // Is there at least one object in the array?
-    if (params.some(p => typeof p == "object")) {
-      ret = "";
-      // Build comma-delimited string
-      for (const item of params) {
-        ret += JSON.stringify(item) + ",";
-      }
     }
 
     return ret;
@@ -78,11 +79,6 @@ export class LogService {
       optionalParams);
   }
 
-  fatal(msg: string, ...optionalParams: any[]) {
-    this.writeToLog(msg, LogLevel.Fatal,
-      optionalParams);
-  }
-
   log(msg: string, ...optionalParams: any[]) {
     this.writeToLog(msg, LogLevel.All,
       optionalParams);
@@ -92,7 +88,7 @@ export class LogService {
                      level: LogLevel,
                      params: any[]) {
     if (this.shouldLog(level)) {
-      let entry: LogEntry = new LogEntry();
+      const entry: LogEntry = new LogEntry();
       entry.message = msg;
       entry.level = level;
       entry.extraInfo = params;
